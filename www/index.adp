d80020c6415a87731333208790b5d3065e44b046
Frank Bergmann <frank.bergmann@project-open.com>
2020-11-03 18:42:08 +0000

    - Implemented CSP (Content Security Policy)

--- www/index.adp
+++ www/index.adp
@@ -4,9 +4,11 @@
 <property name="main_navbar_label">notes</property>
 <property name="left_navbar">@left_navbar_html;literal@</property>
 
-<br>
+<script type="text/javascript" <if @::__csp_nonce@ not nil>nonce="@::__csp_nonce;literal@"</if>>
+window.addEventListener('load', function() {
+     document.getElementById('list_check_all').addEventListener('click', function() { acs_ListCheckAll('notes_list', this.checked) });
+});
+</script>
 
 <listtemplate name="@list_id@"></listtemplate>
-
-<br>
 
--- www/index.tcl
+++ www/index.tcl
@@ -42,10 +42,7 @@ set date_format "YYYY-MM-DD"
 # The columns to be shown in the NotesListPage
 set elements_list {
     note_chk {
-	label "<input type=\"checkbox\" 
-                          name=\"_dummy\" 
-                          onclick=\"acs_ListCheckAll('notes_list', this.checked)\" 
-                          title=\"Check/uncheck all rows\">"
+	label "<input id=list_check_all type=\"checkbox\" name=\"_dummy\" title=\"Check/uncheck all rows\">"
 	display_template {
 	    @note_lines.note_chk;noquote@
 	}
@@ -85,6 +82,7 @@ template::list::create \
     -has_checkboxes \
     -bulk_actions $bulk_actions_list \
     -bulk_action_export_vars { object_id } \
+    -bulk_action_method GET \
     -row_pretty_plural [lang::message::lookup {} intranet-notes.Notes_Items "Items"] \
     -elements $elements_list
 
