72fb6cbae0f58f81d7dc48748a498c2f3b46d034
Frank Bergmann <frank.bergmann@project-open.com>
2020-12-07 13:10:19 +0100

    - Added notes portlet to financial documents

--- sql/postgresql/intranet-notes-create.sql
+++ sql/postgresql/intranet-notes-create.sql
@@ -388,6 +388,31 @@ where plugin_name = 'User Notes';
 
 
 
+-- Create a notes plugin for the InvoiceViewPage
+SELECT im_component_plugin__new (
+	null,				-- plugin_id
+	'im_component_plugin',		-- object_type
+	now(),				-- creation_date
+	null,				-- creation_invoice
+	null,				-- creation_ip
+	null,				-- context_id
+	'Invoice Notes',			-- plugin_name
+	'intranet-notes',		-- package_name
+	'right',			-- location
+	'/intranet-invoices/view',		-- page_url
+	null,				-- view_name
+	90,				-- sort_order
+	'im_notes_component -object_id $invoice_id'	-- component_tcl
+);
+
+update im_component_plugins 
+set title_tcl = 'lang::message::lookup "" intranet-notes.Invoice_Notes "Invoice Notes"'
+where plugin_name = 'Invoice Notes';
+
+
+
+
+
 -----------------------------------------------------------
 -- Menu for Notes
 --
